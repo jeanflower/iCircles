1e0b8d0627028dac5fa89f0fb8d4613e157164ef
Aidan Delaney <aidan@phoric.eu>
2012-10-26 13:26:25 +0100

    * Added a default JSON constructor to AbstractDiagram

--- devel/iCircles/src/icircles/input/AbstractDiagram.java
+++ devel/iCircles/src/icircles/input/AbstractDiagram.java
@@ -8,24 +8,17 @@ import com.fasterxml.jackson.annotation.*;
 // Make ShadowAbstractDiagram appear in serialised JSON as "AbstractDiagram"
 @JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.WRAPPER_OBJECT, property="AbstractDiagram")
 public class AbstractDiagram {
-	@JsonProperty(value="Contours")
 	private List<String> contours;
-	
-	@JsonProperty(value="Zones")
 	private List<Zone> zones;
-
-	@JsonProperty(value="ShadedZones")
 	private List<Zone> shadedZones;
-
-	
-	@JsonProperty(value="Spiders")
 	private List<Spider> spiders;
 	
-	/**
-	public ShadowAbstractDiagram (List<String> contours, 
-			List<Zone> zones,
-			List<Zone> shadedZones,
-			List<Spider> spiders) throws IllegalArgumentException {
+	@JsonCreator
+	public AbstractDiagram (
+			@JsonProperty(value="Contours")    List<String> contours, 
+			@JsonProperty(value="Zones")       List<Zone> zones,
+			@JsonProperty(value="ShadedZones") List<Zone> shadedZones,
+			@JsonProperty(value="Spiders")     List<Spider> spiders) throws IllegalArgumentException {
 		this.contours    = contours;
 		this.zones       = zones;
 		this.shadedZones = shadedZones;
@@ -39,5 +32,4 @@ public class AbstractDiagram {
 	private boolean verify() {
 		return true;
 	}
-	*/
 }
