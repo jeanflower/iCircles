9484be11414f4ace89eb190980de4f2efd8ae3b9
Aidan Delaney <a.j.delaney@brighton.ac.uk>
2012-10-30 09:32:24 +0000

    Fixed build issue   - Hadn't updated input.Spider.toAbstractSpider to take a Set of contours

--- devel/iCircles/src/icircles/input/Spider.java
+++ devel/iCircles/src/icircles/input/Spider.java
@@ -3,6 +3,7 @@ package icircles.input;
 import icircles.abstractDescription.*;
 
 import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -14,10 +15,10 @@ public class Spider {
 	@JsonProperty(value="habitat")
 	private List <Zone> habitat;
 	
-	public AbstractSpider toAbstractSpider () {
+	public AbstractSpider toAbstractSpider (Set <AbstractCurve> contours) {
 		TreeSet<AbstractBasicRegion> feet = new TreeSet<AbstractBasicRegion>();
 		for(Zone z : habitat) {
-			feet.add(z.toAbstractBasicRegion());
+			feet.add(z.toAbstractBasicRegion(contours));
 		}
 		
 		return new AbstractSpider(feet, name);
